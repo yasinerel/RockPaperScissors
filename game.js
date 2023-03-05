1ebe6501ddc5f558d2d5fe7562e0a8be3422960f
yasinerel <terel19@ku.edu.tr>
2023-03-05 22:26:37 +0300

    playRound added

--- game.js
+++ game.js
@@ -2,15 +2,41 @@ console.log("yasins");
 
 function getComputerChoice() {
     //Rock’, ‘Paper’ or ‘Scissors’
-    const array = ["Rock", "Paper" ,  "Scissors"];
+    const array = ["rock", "paper" ,  "scissors"];
     const randomIndex = Math.floor(Math.random() * array.length);
     const item = array[randomIndex];
 
     return item;
   }
 
-function playTheGame(computerChoice, playerChoice){
-    let playerChoice = prompt("Choose: Rock, Paper or Scissors");
-    let computerChoice = getComputerChoice();
-    
+function playRound(playerSelection, computerSelection){
+
+    if(playerSelection == "rock" && computerSelection == "rock" ){
+        console.log("Deal! Again");
+    }if(playerSelection == "rock" && computerSelection == "paper" ){
+        console.log("Computer Win! Paper beats Rock");
+    }if(playerSelection == "rock" && computerSelection == "scissors" ){
+        console.log("Player Win! Rock beats Scissors");
+    }if(playerSelection == "paper" && computerSelection == "rock" ){
+        console.log("Player Win! Paper beats Rock");
+    }if(playerSelection == "paper" && computerSelection == "paper" ){
+        console.log("Deal! Again");
+    }if(playerSelection == "paper" && computerSelection == "scissors" ){
+        console.log("Computer Win! Scissors beats Paper");
+    }if(playerSelection == "scissors" && computerSelection == "rock" ){
+        console.log("Computer Win! Rock beats Scissors");
+    }if(playerSelection == "scissors" && computerSelection == "paper" ){
+        console.log("Player Win! Scissors beats Paper");
+    }if(playerSelection == "scissors" && computerSelection == "scissors" ){
+        console.log("Deal! Again");
+    }
+
+
 }
+
+//let playerSelection = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
