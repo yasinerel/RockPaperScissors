6ea866e22185cfe390ce00bed96a207890f73ac8
yasinerel <terel19@ku.edu.tr>
2023-03-05 22:55:21 +0300

    general skeleton done

--- game.js
+++ game.js
@@ -1,4 +1,3 @@
-console.log("yasins");
 
 function getComputerChoice() {
     //Rock’, ‘Paper’ or ‘Scissors’
@@ -9,34 +8,58 @@ function getComputerChoice() {
     return item;
   }
 
+let player = 0;
+let computer = 0;
+
 function playRound(playerSelection, computerSelection){
 
     if(playerSelection == "rock" && computerSelection == "rock" ){
-        console.log("Deal! Again");
+        console.log("Draw! Again");
     }if(playerSelection == "rock" && computerSelection == "paper" ){
         console.log("Computer Win! Paper beats Rock");
+        computer ++;
     }if(playerSelection == "rock" && computerSelection == "scissors" ){
         console.log("Player Win! Rock beats Scissors");
+        player ++;
     }if(playerSelection == "paper" && computerSelection == "rock" ){
         console.log("Player Win! Paper beats Rock");
+        player ++;
     }if(playerSelection == "paper" && computerSelection == "paper" ){
-        console.log("Deal! Again");
+        console.log("Draw! Again");
     }if(playerSelection == "paper" && computerSelection == "scissors" ){
         console.log("Computer Win! Scissors beats Paper");
+        computer ++;
     }if(playerSelection == "scissors" && computerSelection == "rock" ){
         console.log("Computer Win! Rock beats Scissors");
+        computer ++;
     }if(playerSelection == "scissors" && computerSelection == "paper" ){
         console.log("Player Win! Scissors beats Paper");
+        player ++;
     }if(playerSelection == "scissors" && computerSelection == "scissors" ){
-        console.log("Deal! Again");
+        console.log("Draw! Again");
     }
+}
 
+
+
+//console.log(playRound(playerSelection, computerSelection));
+
+
+ function game(){
+    const result = [];
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
+        const computerSelection = getComputerChoice().toLowerCase();
+        playRound(playerSelection,computerSelection);
+
+        console.log("Player's score is " + player + " Computer's score is " + computer);
+
+    }
 
 }
 
-//let playerSelection = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+
+
+game();
