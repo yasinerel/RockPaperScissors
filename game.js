4cb25c59da9185af17c4b7cf12dc806ae4e7977f
yasinerel <terel19@ku.edu.tr>
2023-03-07 23:32:25 +0300

    general dynamics ok

--- game.js
+++ game.js
@@ -15,74 +15,81 @@ function playRound(playerSelection, computerSelection){
 
     if(playerSelection == "rock" && computerSelection == "rock" ){
         console.log("Draw! Again");
+        results();
     }if(playerSelection == "rock" && computerSelection == "paper" ){
         console.log("Computer Win! Paper beats Rock");
-        computer ++;
+        computer = computer + 1;
+        results();
     }if(playerSelection == "rock" && computerSelection == "scissors" ){
         console.log("Player Win! Rock beats Scissors");
-        player ++;
+        player = player + 1;
+        results();
     }if(playerSelection == "paper" && computerSelection == "rock" ){
         console.log("Player Win! Paper beats Rock");
-        player ++;
+        player = player + 1;
+        results();
     }if(playerSelection == "paper" && computerSelection == "paper" ){
         console.log("Draw! Again");
+        results();
     }if(playerSelection == "paper" && computerSelection == "scissors" ){
         console.log("Computer Win! Scissors beats Paper");
-        computer ++;
+        computer = computer + 1;
+        results();
     }if(playerSelection == "scissors" && computerSelection == "rock" ){
         console.log("Computer Win! Rock beats Scissors");
-        computer ++;
+        computer = computer + 1;
+        results();
     }if(playerSelection == "scissors" && computerSelection == "paper" ){
         console.log("Player Win! Scissors beats Paper");
-        player ++;
+        player = player + 1;
+        results();
     }if(playerSelection == "scissors" && computerSelection == "scissors" ){
         console.log("Draw! Again");
+        results();
     }
+
+    if(computer == 5){
+        console.log("🏆🏆🏆 COMPUTER WINNER! 🏆🏆🏆");
+    }
+    if(player == 5){
+        console.log("🏆🏆🏆 PLAYER WINNER! 🏆🏆🏆");
+    }
+
 }
-
-
-
 //console.log(playRound(playerSelection, computerSelection));
 
 
  function game(){
-    const result = [];
 
-        let playerSelection = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
-        const computerSelection = getComputerChoice().toLowerCase();
-        playRound(playerSelection,computerSelection);
+    // let playerSelection = prompt("Choose: Rock, Paper or Scissors").toLowerCase();
+    // const computerSelection = getComputerChoice().toLowerCase();
 
-        console.log("Player's score is " + player + " Computer's score is " + computer);
-
-
-
-    if(player < computer){
-        console.log("COMPUTER WINNER!");
-    }
-    if(player > computer){
-        console.log("PLAYER WINNER!");
-    }
-    if(player == computer){
-        console.log("DRAW! ONE MORE GAME?");
-    }
-
+    const btnR = document.querySelector('#rock');
+    btnR.addEventListener('click', () => {
+      playRound("rock", getComputerChoice().toLowerCase());
+      
+    });
+    
+    
+    const btnP = document.querySelector('#paper');
+    btnP.addEventListener('click', () => {
+        playRound("paper", getComputerChoice().toLowerCase());
+    });
+    
+    const btnS = document.querySelector('#scissors');
+    btnS.addEventListener('click', () => {
+        playRound("scissors", getComputerChoice().toLowerCase());
+    });
+    //playRound(playerSelection,computerSelection);
 }
 
-//game();
 
-const btnR = document.querySelector('#rock');
-btnR.addEventListener('click', () => {
-  console.log(playRound("rock", getComputerChoice().toLowerCase()));
-  
-});
+function results(){
+    console.log("Player's score is " + player + " Computer's score is " + computer);
+    console.log("*********||*********");
+    // console.log(computer);
+    // console.log(player);
+}
 
+game();
 
-const btnP = document.querySelector('#paper');
-btnP.addEventListener('click', () => {
-    console.log(playRound("paper", getComputerChoice().toLowerCase()));
-});
-
-const btnS = document.querySelector('#scissors');
-btnS.addEventListener('click', () => {
-    console.log(playRound("scissors", getComputerChoice().toLowerCase()));
-});
